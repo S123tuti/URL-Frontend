5f4019f74f36a355af87ad36bfed21401ca2353b
s123tuti <stutisharmaofficial@gmail.com>
2023-05-22 22:24:54 +0530

    partial

--- url-shortner/src/App.js
+++ url-shortner/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import BackgroundAnimate from './BackgroundAnimate';
+import InputShortener from './InputShortener';
+import LinkResult from './LinkResult';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+
   return (
-    <div className="App">
-      hello
+    <div className="container">
+      <InputShortener setInputValue={setInputValue} />
+      <BackgroundAnimate />
+      <LinkResult inputValue={inputValue} />
     </div>
   );
 }
